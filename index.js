d7ead357a6c8c71f374747ca4aaaf611f53a26b4
Aura <auraesteban@gmail.com>
2025-07-15 16:03:43 +0800

    Add missing minutes

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ const numberWordsDict = {
   10: 'ten',
   11: 'eleven',
   12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
   15: 'quarter',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  21: 'twenty one',
+  22: 'twenty two',
+  23: 'twenty three',
+  24: 'twenty four',
+  25: 'twenty five',
+  26: 'twenty six',
+  27: 'twenty seven',
+  28: 'twenty eight',
+  29: 'twenty nine',
 };
 
 // expecting time to be a string in the format like '8:15' or '12:30'
--- index.test.js
+++ index.test.js
@@ -25,4 +25,14 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles other minutes - 2:47', () => {
+    const timeInWords = convertTimeToWords('2:47');
+    expect(timeInWords).toBe('thirteen to three');
+  });
+
+  it('Handles other minutes - 12:18', () => {
+    const timeInWords = convertTimeToWords('12:18');
+    expect(timeInWords).toBe('eighteen past twelve');
+  });
 });
