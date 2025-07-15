aeab320d09248d5073b19217133ea81bbc8b04da
Aura <auraesteban@gmail.com>
2025-07-15 14:03:28 +0800

    Remove TODO comment

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const numberWordsDict = {
 
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
   }
